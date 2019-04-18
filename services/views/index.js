f73b2028ada3890f49452c591351f0cadceba293
Arash Paknezad <a.paknezad@hotmail.com>
2019-04-19 00:35:13 +0200

    changed comment rendering

--- services/views/index.js
+++ services/views/index.js
@@ -3,9 +3,9 @@ const fs = require('fs');
 const path = require('path');
 
 const comment = fs.readFileSync(path.join(__dirname, '../../views/partials/comment.hbs'), 'utf8');
+const commentTemplate = Handlebars.compile(comment);
 
 const getCommentNode = data => {
-    const commentTemplate = Handlebars.compile(comment);
     return commentTemplate(data);
 };
 
