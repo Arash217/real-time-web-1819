e5c5a321a084907605b5c6fcf70f595a5d205327
Arash Paknezad <a.paknezad@hotmail.com>
2019-04-19 20:50:00 +0200

    changed layout

--- public/js/main.js
+++ public/js/main.js
@@ -3,11 +3,23 @@ const socket = io();
 const filterForm = document.getElementById('filter-form');
 const comments = document.getElementById('comments');
 
-filterForm.addEventListener('submit', e => {
+const debounce = (fn, wait) => {
+    let timeout;
+    return (...args) => {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+            fn.apply(this, args);
+        }, wait);
+    };
+};
+
+const inputEventHandler = e => {
     e.preventDefault();
     const filterFormData = new FormData(filterForm);
     socket.emit('filter', filterFormData.get('filter'));
-});
+};
+
+filterForm.addEventListener('input', debounce(inputEventHandler, 300));
 
 socket.on('comment', comment => {
     comments.insertAdjacentHTML('afterbegin', comment);
