2ec37b4cef51925f94a498b7483f30d62d183832
Arash Paknezad <a.paknezad@hotmail.com>
2019-05-20 07:26:10 +0200

    filter api fields

--- services/reddit/stream.js
+++ services/reddit/stream.js
@@ -1,5 +1,5 @@
 const EventSource = require('eventsource');
 
-const eventSource = new EventSource('http://stream.pushshift.io/?type=comments&over_18=true');
+const eventSource = new EventSource('http://stream.pushshift.io/?type=comments&over_18=true&filter=permalink,subreddit_name_prefixed,author,body');
 
 module.exports = eventSource;
